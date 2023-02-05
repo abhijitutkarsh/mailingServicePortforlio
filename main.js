9f9dc57f90293e0b42829f87301ada4f51746580
Abhijit Utkarsh <abhijitu18@gmail.com>
2023-02-05 19:36:41 +0530

    cors

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ app.use(express.json());
  app.use(
     cors({
       // origin: ["https://ecommerce370001.herokuapp.com"],
-      origin:["http://localhost:3000"], 
+      origin:["http://localhost:3000", "https://react-portfolio-bft6-psho6m2z4-abhijitutkarsh.vercel.app/"], 
       methods: ["GET", "POST"],
       credentials: true,
     })
