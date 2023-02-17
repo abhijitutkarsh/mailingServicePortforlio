9752c54d4290a55677649ace3fcfd5f6cd8897b3
Abhijit Utkarsh <abhijitu18@gmail.com>
2023-02-18 00:23:09 +0530

    yes

--- main.js
+++ main.js
@@ -23,7 +23,7 @@ app.use(express.json());
   
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Credentials", true);
-  res.header("Access-Control-Allow-Origin", req.headers.origin);
+  res.header("Access-Control-Allow-Origin", 'http://194.163.40.137:3000');
   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
   res.header(
     "Access-Control-Allow-Headers",
