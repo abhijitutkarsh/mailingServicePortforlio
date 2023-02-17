4bc3eec4c9ec4b1e58654dc1ceebedbc090b9911
Abhijit Utkarsh <abhijitu18@gmail.com>
2023-02-18 00:14:21 +0530

    yes

--- main.js
+++ main.js
@@ -20,6 +20,19 @@ app.use(express.json());
       credentials: true,
     })
   );
+  
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Origin", req.headers.origin);
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept"
+  );
+  next();
+});
+app.enable('trust proxy');
+
  //for form data
 //  app.use(session({
 //    secret: 'keyboard cat',
